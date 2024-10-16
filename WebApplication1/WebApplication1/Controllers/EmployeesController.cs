f76a1bff506dd6359cdf464357ca300833a306c6
Ingvar Derešivski <inde999@gmail.com>
2024-10-16 11:39:50 +0300

    StoredProcedure

---
+++ WebApplication1/WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProc.Data;
+using StoredProc.Models;
+
+
+namespace StoredProc.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public StoredProcDbContext _context;
+
+        public EmployeesController
+            (
+            StoredProcDbContext context
+            )
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                .FromSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
+        }
+    }
+}
